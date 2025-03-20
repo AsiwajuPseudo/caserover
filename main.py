16967d53553e4bbaedcb01aeb42cf8fa4fb858b2
Munyaradzi Togarepi <directorpseudo@gmail.com>
2025-03-20 11:07:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -318,8 +318,11 @@ def run_playground():
       answer = tools.extracter(prompt, 4060, text, history)
       sources = files
     else:
+      document=''
+      if 'document' in data:
+        document=data.get('document')
       history = database.messages(chat)
-      answer, sources = tools.rag(tool, prompt, history, 3, 4060)
+      answer, sources = tools.rag(tool, prompt, document history, 3, 4060)
   except Exception as e:
     print(e)
     p={"answer":[{"type":"paragraph","data":"Error generating content, please try again. If the error persist create a new workspace."}],"sources":[], "citations":[]}
